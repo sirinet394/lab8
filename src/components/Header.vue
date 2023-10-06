df7defbefd5c0fc746c1a08c47bb5086d0222a23
sirinet394 <141398718+sirinet394@users.noreply.github.com>
2023-10-06 21:14:07 +0700

    Update Header.vue

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,7 +1,7 @@
 <template>
     <nav class="navbar navbar-expand-lg rounded navbar-light bg-light">
         <div class="container-fluid">
-            <router-link class="navbar-brand" to="/">Gaming Shop</router-link>
+            <router-link class="navbar-brand" to="/">Yarn Shop</router-link>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
             </button>
